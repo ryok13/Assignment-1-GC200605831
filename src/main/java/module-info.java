c9c129771fbae75dc80e4d8f2a1a0c1ff4b5a8c6
ryok13 <mario24ryok@gmail.com>
2025-06-01 19:32:55 -0400

    Connect to Git repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.georgiancollege.assignment1gc200605831 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.georgiancollege.assignment1gc200605831 to javafx.fxml;
+    exports com.georgiancollege.assignment1gc200605831;
+}
